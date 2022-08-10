975cc6a37a24b5141a0483dc740eb84cd8fc29b2
Rodin Grajo <rodin534@revature.net>
2022-08-10 00:31:28 -0500

    fixed typo

--- ChuckNorrisJokes/Joke.cs
+++ ChuckNorrisJokes/Joke.cs
@@ -6,7 +6,7 @@ using System.Text.Json.Serialization;
 
 namespace ChuckNorrisJokes
 {
-    public class Jokes
+    public class Joke
     {
         [JsonPropertyName("type")]
         public string Type { get; set; }
