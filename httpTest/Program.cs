30c37d1737c9f334be923aa03fa7f74b647c38d8
Rodin Grajo <rodin534@revature.net>
2022-08-09 22:16:31 -0500

    added http test

---
+++ httpTest/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace httpTest
+{
+    class Program
+    {
+        static readonly HttpClient client = new HttpClient();
+        static async Task Main()
+        {
+        // Call asynchronous network methods in a try/catch block to handle exceptions.
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync("http://api.icndb.com/jokes/random?limitTo=[nerdy] ");
+                response.EnsureSuccessStatusCode();
+                HttpResponseMessage r = await client.GetAsync("http://api.icndb.com/jokes/random?exclude=Array");
+                r.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                string rbody = await r.Content.ReadAsStringAsync();
+                System.Console.WriteLine(rbody);
+                // Above three lines can be replaced with new helper method below
+                // string responseBody = await client.GetStringAsync(uri);
+                Console.WriteLine(responseBody);
+            }
+            catch(HttpRequestException e)
+            {
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine("Message :{0} ",e.Message);
+            }
+        }
+    }
+}
